9cd51b000f5c95bddaa86751088d3ecff0fe85e7
henriquedeantoni <henriqdeantoni@gmail.com>
2025-03-22 20:13:34 -0300

    feat: dal class completada

--- ParkingLot.Data/Data/DAL.cs
+++ ParkingLot.Data/Data/DAL.cs
@@ -8,6 +8,39 @@ namespace ParkingLot.Data.Data
 {
     public class DAL<T> where T: class
     {
+        protected readonly ParkingLotContext context;
 
+        public DAL(ParkingLotContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<T> List()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void AddToList(T genObject)
+        {
+            context.Set<T>().Add(genObject);
+            context.SaveChanges();
+        }
+
+        public void updateFromList(T genObject)
+        {
+            context.Set<T>().Update(genObject);
+            context.SaveChanges();
+        }
+
+        public void RemoveFromList(T genObject)
+        {
+            context.Set<T>().Remove(genObject);
+            context.SaveChanges();
+        }
+
+        public T? GetBy(Func<T, bool> condition)
+        {
+            return context.Set<T>().FirstOrDefault(condition);
+        }
     }
 }
