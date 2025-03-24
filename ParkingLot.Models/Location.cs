ab9273d5c21f40c12f044b7a1d3d3a80725b7eda
henriquedeantoni <henriqdeantoni@gmail.com>
2025-03-23 22:38:06 -0300

    feat: incluida nova classe

---
+++ ParkingLot.Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string LocationNumber { get; set; }
+        public string Status { get; set; }
+        public Location(int id, string locationNumber, string status)
+        {
+            Id = id;
+            LocationNumber = locationNumber;
+            Status = status;
+        }
+    }
+}
