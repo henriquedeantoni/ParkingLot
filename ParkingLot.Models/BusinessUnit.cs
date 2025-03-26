f16d4f91316128d08d933250edb79591328f6975
henriquedeantoni <henriqdeantoni@gmail.com>
2025-03-26 00:13:52 -0300

    feat: incluido propriedades classe

--- ParkingLot.Models/BusinessUnit.cs
+++ ParkingLot.Models/BusinessUnit.cs
@@ -8,6 +8,9 @@ namespace ParkingLot.Models
 {
     internal class BusinessUnit
     {
-
+        public virtual ICollection<Location> Locations { get; set; } = new List<Location>(); 
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public BusinessUnit() { }
     }
 }
