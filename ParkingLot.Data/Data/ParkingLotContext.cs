23191a0dd4b3920ed95510b5b1a9eee2a3be0f60
henriquedeantoni <henriqdeantoni@gmail.com>
2025-03-27 00:48:28 -0300

    feat: atualizado context com as relações entre tabelas

--- ParkingLot.Data/Data/ParkingLotContext.cs
+++ ParkingLot.Data/Data/ParkingLotContext.cs
@@ -13,7 +13,7 @@ namespace ParkingLot.Data.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<ParkingSession> ParkingSessions { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
-        public DbSet<ParkingSpot> Locations { get; set; }
+        public DbSet<ParkingSpot> ParkingSpots { get; set; }
         public DbSet<BusinessUnit> BusinessUnits { get; set; }
 
         private string connection = EnvConfig.GetEnvVariable("STRING_CONNECTION");
@@ -32,6 +32,10 @@ namespace ParkingLot.Data.Data
             modelBuilder.Entity<Customer>()
                         .HasMany(c => c.ParkingSessions)
                         .WithOne(c => c.Customer);
+
+            modelBuilder.Entity<BusinessUnit>()
+                        .HasMany(c => c.ParkingSpots)
+                        .WithOne(c => c.BusinessUnit);
 
         }
 
